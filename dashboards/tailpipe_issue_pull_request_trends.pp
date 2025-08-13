f61fa3b852da5fbefa3a7246c356f1fa2a012445
Ved misra <47312748+misraved@users.noreply.github.com>
2025-08-13 19:57:28 +0530

    Add tailpipe repository checks (#6)

---
+++ dashboards/tailpipe_issue_pull_request_trends.pp
@@ -0,0 +1,156 @@
+dashboard "tailpipe_issue_pull_request_age_trends" {
+
+  title = "Tailpipe Issue and Pull Request Age Trends"
+
+  tags = merge(local.github_common_tags, {
+    type = "Dashboard"
+  })
+
+  container {
+
+    chart {
+      title = "Core Issues Total Age (Days)"
+      type  = "line"
+      query = query.tailpipe_core_issue_trend
+      width = 6
+    }
+
+    chart {
+      title = "Core Pull Requests Total Age (Days)"
+      type  = "line"
+      query = query.tailpipe_core_pull_request_trend
+      width = 6
+    }
+
+    chart {
+      title = "Mod Issues Total Age (Days)"
+      type  = "line"
+      query = query.tailpipe_mod_issue_trend
+      width = 6
+    }
+
+    chart {
+      title = "Mod Pull Requests Total Age (Days)"
+      type  = "line"
+      query = query.tailpipe_mod_pull_request_trend
+      width = 6
+    }
+
+    chart {
+      title = "Plugin Issues Total Age (Days)"
+      type  = "line"
+      query = query.tailpipe_plugin_issue_trend
+      width = 6
+    }
+
+    chart {
+      title = "Plugin Pull Requests Total Age (Days)"
+      type  = "line"
+      query = query.tailpipe_plugin_pull_request_trend
+      width = 6
+    }
+  }
+
+}
+
+query "tailpipe_core_issue_trend" {
+  sql = <<-EOQ
+    select
+      created_at as "Date",
+      sum((r ->> 'Age in Days')::numeric) as "Days Open"
+    from
+      pipes_workspace_snapshot,
+      jsonb_array_elements(data -> 'panels' -> 'community_tracker.table.container_dashboard_tailpipe_core_issue_age_report_anonymous_container_0_anonymous_table_0' -> 'data' -> 'rows') as r
+    where
+      dashboard_name = 'community_tracker.dashboard.tailpipe_core_issue_age_report'
+    group by
+      created_at
+    order by
+      created_at
+  EOQ
+}
+
+query "tailpipe_core_pull_request_trend" {
+  sql = <<-EOQ
+    select
+      created_at as "Date",
+      sum((r ->> 'Age in Days')::numeric) as "Days Open"
+    from
+      pipes_workspace_snapshot,
+      jsonb_array_elements(data -> 'panels' -> 'community_tracker.table.container_dashboard_tailpipe_core_pull_request_age_report_anonymous_container_0_anonymous_table_0' -> 'data' -> 'rows') as r
+    where
+      dashboard_name = 'community_tracker.dashboard.tailpipe_core_pull_request_age_report'
+    group by
+      created_at
+    order by
+      created_at
+  EOQ
+}
+
+query "tailpipe_mod_issue_trend" {
+  sql = <<-EOQ
+    select
+      created_at as "Date",
+      sum((r ->> 'Age in Days')::numeric) as "Days Open"
+    from
+      pipes_workspace_snapshot,
+      jsonb_array_elements(data -> 'panels' -> 'community_tracker.table.container_dashboard_tailpipe_mod_issue_age_report_anonymous_container_0_anonymous_table_0' -> 'data' -> 'rows') as r
+    where
+      dashboard_name = 'community_tracker.dashboard.tailpipe_mod_issue_age_report'
+    group by
+      created_at
+    order by
+      created_at
+  EOQ
+}
+
+query "tailpipe_mod_pull_request_trend" {
+  sql = <<-EOQ
+    select
+      created_at as "Date",
+      sum((r ->> 'Age in Days')::numeric) as "Days Open"
+    from
+      pipes_workspace_snapshot,
+      jsonb_array_elements(data -> 'panels' -> 'community_tracker.table.container_dashboard_tailpipe_mod_pull_request_age_report_anonymous_container_0_anonymous_table_0' -> 'data' -> 'rows') as r
+    where
+      dashboard_name = 'community_tracker.dashboard.tailpipe_mod_pull_request_age_report'
+    group by
+      created_at
+    order by
+      created_at
+  EOQ
+}
+
+query "tailpipe_plugin_issue_trend" {
+  sql = <<-EOQ
+    select
+      created_at as "Date",
+      sum((r ->> 'Age in Days')::numeric) as "Days Open"
+    from
+      pipes_workspace_snapshot,
+      jsonb_array_elements(data -> 'panels' -> 'community_tracker.table.container_dashboard_tailpipe_plugin_issue_age_report_anonymous_container_0_anonymous_table_0' -> 'data' -> 'rows') as r
+    where
+      dashboard_name = 'community_tracker.dashboard.tailpipe_plugin_issue_age_report'
+    group by
+      created_at
+    order by
+      created_at
+  EOQ
+}
+
+query "tailpipe_plugin_pull_request_trend" {
+  sql = <<-EOQ
+    select
+      created_at as "Date",
+      sum((r ->> 'Age in Days')::numeric) as "Days Open"
+    from
+      pipes_workspace_snapshot,
+      jsonb_array_elements(data -> 'panels' -> 'community_tracker.table.container_dashboard_tailpipe_plugin_pull_request_age_report_anonymous_container_0_anonymous_table_0' -> 'data' -> 'rows') as r
+    where
+      dashboard_name = 'community_tracker.dashboard.tailpipe_plugin_pull_request_age_report'
+    group by
+      created_at
+    order by
+      created_at
+  EOQ
+}
